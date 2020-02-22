f576e40b70ae7bb02870296dc0a6eddb62a3c0bd
louis <louis.hoeser@gmail.com>
2020-02-22 23:48:25 +0100

    basic sequencer works know, added BPM and Volume Control, improved Usabilty

--- drumcomputer/src/app/sequencer/sequencer.component.ts
+++ drumcomputer/src/app/sequencer/sequencer.component.ts
@@ -9,7 +9,10 @@ import * as Tone from "tone";
 export class SequencerComponent implements OnInit {
 
   sound: any = new Tone.Player("assets/samples/drums/kicks/Kick011TheSpoon.mp3").toMaster();
-  
+  sequence: any;
+  sequenceRunning: boolean = false; // Flag for Play button
+  bpm: number = 120;
+  volume: number = -12;
 
   states: boolean[] = [false, false, false, false,
     false, false, false, false,
@@ -19,24 +22,56 @@ export class SequencerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // sets the Volume
-    this.sound.volume.value = -12;
+    this.sound.volume.value = this.volume;
+    this.sound.fadeOut = "64n"; 
   }
 
   activateCell(i: number) {
+    // stop sequence if initialized
+    if (this.sequence!=undefined) {
+      this.sequence.stop();
+      Tone.Transport.stop();
+      this.sequenceRunning = false;
+    }
+    // flip State
     this.states[i] = !this.states[i];
   }
 
   test() {
-    this.updateSequencer();
-    Tone.Transport.start();
+    if(!this.sequenceRunning) {
+      this.updateSequencer();
+      this.sequence.start();
+      Tone.Transport.start();
+      this.sequenceRunning = true;
+    } else {
+      this.sequence.stop();
+      Tone.Transport.stop();
+      this.sequenceRunning = false;
+    }    
   }
 
   updateSequencer() {
-    let sequence: any = new Tone.Sequence(function(time, col){
+    let snd = this.sound; // safe reference to sound for anon function 
+
+    this.sequence = new Tone.Sequence(function(time, col){
       if(col) {
-        this.sound.start(time, 0, "16n");
+        snd.start(time, 0, "16n");
       }
-    }, this.states, "16n").start();
+
+      Tone.Draw.schedule(function(){
+
+      }, time);
+
+    }, this.states, "16n");
+  }
+
+  changeBpm(value: number) {
+    this.bpm = value;
+    Tone.Transport.bpm.value = this.bpm;
+  }
+
+  changeVolume(value: number) {
+    this.volume = value;
+    this.sound.volume.value = this.volume;
   }
 }
